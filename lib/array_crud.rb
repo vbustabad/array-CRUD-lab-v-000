ea6557a62d738685cb4a67a7faed132e3ffdffe0
Veronica Bustabad <vbustabad@gmail.com>
2018-01-26 02:04:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Monday", "Tuesday", "Wednesday", "Thursday"]
+  ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
