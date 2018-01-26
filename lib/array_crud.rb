aaefad219c87caa9483c95ba189499ebd397cf22
Veronica Bustabad <vbustabad@gmail.com>
2018-01-26 02:02:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("Sunday") 
+  create_an_array.unshift("Sunday")
 end
 
 def remove_element_from_end_of_array(array)
-
+  
 end
 
 def remove_element_from_start_of_array(array)
